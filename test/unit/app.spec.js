ec193bdc2d3f098ce427d609df8a17ff8e2a3a16
aniaw <ania_w@outolook.com>
2016-05-07 10:47:03 +0200

    add new test cases

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -8,18 +8,26 @@ describe('app', function () {
             expect(app.assignToSwimmingCourse('Jack', '10/10/1995')).toEqual({
                 name: 'Jack', age: 20, course: 'adults'
             });
+            expect(app.assignToSwimmingCourse('Maria', '11/10/1997')).toEqual({
+                name: "Maria", age: 18, course: 'adults'
+            });
         });
-        it('should assign person between the ages of 12 - 17 to teens group', function () {
+        xit('should assign person between the ages of 12 - 17 to teens group', function () {
             expect(app.assignToSwimmingCourse('John', '07/10/2001')).toEqual({
                 name: 'John', age: 14, course: 'teens'
             });
+            expect(app.assignToSwimmingCourse('Bella', '06/04/2002')).toEqual({
+                name: 'Bella', age: 13, course: 'teens'
+            });
         });
-        it('should assign person below 12 to kids group', function () {
+        iit('should assign person below 12 to kids group', function () {
             expect(app.assignToSwimmingCourse('Zack', '10/06/2010')).toEqual({
                 name: 'Zack', age: 5, course: 'kids'
             });
         });
+        it('should should...', function () {
+
+        });
     });
 });
-
 
